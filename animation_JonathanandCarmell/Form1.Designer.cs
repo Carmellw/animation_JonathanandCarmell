8894c8b3c905d2088f53b61348eb5b3d6f07d67c
Carmellw <carmellwass@gmail.com>
2020-12-11 17:32:48 +0200

    final

--- animation_JonathanandCarmell/Form1.Designer.cs
+++ animation_JonathanandCarmell/Form1.Designer.cs
@@ -29,40 +29,154 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(390, 84);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(5, 60);
-            this.label1.TabIndex = 0;
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
-            this.timer1.Interval = 10;
+            this.timer1.Interval = 30;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.Transparent;
+            this.Ball.Image = ((System.Drawing.Image)(resources.GetObject("Ball.Image")));
+            this.Ball.Location = new System.Drawing.Point(-150, 170);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(140, 137);
+            this.Ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Ball.TabIndex = 1;
+            this.Ball.TabStop = false;
+            this.Ball.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(352, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 60);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(951, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(300, 60);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(122, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 60);
+            this.label3.TabIndex = 4;
+            this.label3.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(-25, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(229, 60);
+            this.label4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(729, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(245, 60);
+            this.label5.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(432, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(178, 60);
+            this.label6.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(1166, 47);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(140, 51);
+            this.label7.TabIndex = 8;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(795, 64);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(168, 60);
+            this.label8.TabIndex = 9;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Chartreuse;
+            this.label9.Location = new System.Drawing.Point(-97, 684);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(1516, 60);
+            this.label9.TabIndex = 10;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(1374, 729);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- animation_JonathanandCarmell/Form1.cs
+++ animation_JonathanandCarmell/Form1.cs
@@ -13,21 +13,76 @@ namespace animation_JonathanandCarmell
 
     public partial class Form1 : Form
     {
-        private double gravity = 10;
-        private int constant = 5;
-        private double mass = 1;
-        private double force;
+        private double gravity = 0.0001;
+        private double mass = 10;
+        private double forceY;
+        private double forceX = 3;
+        private double time = 1;
+        private double vY = 0;
+        private double vX = 1;
+        private Label[] clouds = new Label[8];
+
 
         public Form1()
         {
             InitializeComponent();
+            Setarr();
+        }
+
+        public void Setarr()
+        {
+            clouds[0] = label1;
+            clouds[1] = label2;
+            clouds[2] = label3;
+            clouds[3] = label4;
+            clouds[4] = label5;
+            clouds[5] = label6;
+            clouds[6] = label7;
+            clouds[7] = label8;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+                vY = vY + gravity * time;
+                if (Ball.Top + vY * time <= 285)
+                {
+                    Ball.Top += Convert.ToInt32(vY * time);
+                    time++;
+                }
+                else
+                {
+                    vY = -0.75 * vY;
+                    forceX = 0.95 * forceX;
+                }
+
+                vX = forceX;
+                Ball.Left += Convert.ToInt32(vX);
             
-            force = mass*gravity;
-            label1.Height += Convert.ToInt32(force);
+
+             
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < clouds.Length; i++)
+            {
+                if (clouds[i].Left < 600)
+                {
+                    clouds[i].Left += 1;
+                }
+                else
+                {
+                    clouds[i].Left = -100;
+                }
+            }
         }
     }
 }
